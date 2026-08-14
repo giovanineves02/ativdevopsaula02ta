6df8b1eee4a4d3b0d75c8eea229e84988522372d
giovanineves02 <gioneves17@gmail.com>
2026-08-13 22:39:39 -0300

    Aula 02 devops primeira pipeline

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main (String[] args){
+        System.out.println("Olá, Devops");
+        System.out.println("Pipeline executada com sucesso!");
+    }
+
+}
