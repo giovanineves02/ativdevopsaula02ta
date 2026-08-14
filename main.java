98b914502ab86e34ab8b9c2c35d4449672fb6a23
giovanineves02 <gioneves17@gmail.com>
2026-08-13 22:50:55 -0300

    Alteração da mensagem

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main (String[] args){
         System.out.println("Olá, Devops");
         System.out.println("Pipeline executada com sucesso!");
+        System.out.println("Giovani Neves");
     }
 
 }
